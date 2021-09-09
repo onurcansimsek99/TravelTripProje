7ff261b0bebb5d16dec624118d1c6c9ffcf51020
onurcan şimşek <1030522873@erciyes.edu.tr>
2021-09-10 00:30:54 +0300

    Values ​​were pulled from the database to the blog page.

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+    }
+}
