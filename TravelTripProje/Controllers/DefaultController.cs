21d1229118ce5be98d572e0989284f8d2c2f49ad
onurcan şimşek <1030522873@erciyes.edu.tr>
2021-09-11 01:39:13 +0300

    Featured blogs section on the homepage has been edited.

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
 
         public ActionResult About()
         {
             return View();
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();         // Z' den A' ya sırala ve 2 değeri al, listele.
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(deger);
         }
 
     }
